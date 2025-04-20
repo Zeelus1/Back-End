2d826ce0a8cefeb1704af538ef96904b66b9854c
AndreyJustino <andreyjustino2020@gmail.com>
2025-04-20 20:12:54 -0300

    first commit

---
+++ src/main/java/com/zeelus/zeelus/ZeelusApplication.java
@@ -0,0 +1,13 @@
+package com.zeelus.zeelus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeelusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeelusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeelus/zeelus/ZeelusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeelus.zeelus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeelusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
