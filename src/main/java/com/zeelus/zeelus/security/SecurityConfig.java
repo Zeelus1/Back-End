dffe9f233db9715593c6481fbd4d36c6ea7bfbc4
AndreyJustino <andreyjustino2020@gmail.com>
2025-06-06 14:23:11 -0300

    fix: concertando SecurityConfig para autenticar fluxo e permissões especificas para requisicao de POST

--- src/main/java/com/zeelus/zeelus/security/SecurityConfig.java
+++ src/main/java/com/zeelus/zeelus/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.zeelus.zeelus.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,14 +20,17 @@ public class SecurityConfig {
     private SecurityFilterAcompanhante securityFilterAcompanhante;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .authorizeHttpRequests((auth) -> {
-                    auth.requestMatchers("/acompanhante").permitAll()
-                            .anyRequest().authenticated();
-                }).addFilterBefore(securityFilterAcompanhante, BasicAuthenticationFilter.class);
-
-        return httpSecurity.build();
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.POST, "/acompanhante/cadastrar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/acompanhante/login").permitAll()
+                        .anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilterAcompanhante, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
