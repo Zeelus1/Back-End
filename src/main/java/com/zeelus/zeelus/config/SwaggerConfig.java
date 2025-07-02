9f42a708c7af445b038511c8141f81b24771a880
AndreyJustino <andreyjustino2020@gmail.com>
2025-07-01 21:53:38 -0300

    feat: atualizar configuração do Swagger e adicionar documentação para o endpoint de cadastro de acompanhado

--- src/main/java/com/zeelus/zeelus/config/SwaggerConfig.java
+++ src/main/java/com/zeelus/zeelus/config/SwaggerConfig.java
@@ -1,29 +1,40 @@
 package com.zeelus.zeelus.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    private SecurityScheme securityScheme(){
-        return new SecurityScheme()
-                .name("JWT_Auth")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-    }
 
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Cursos de Programação")
-                        .version("1")
-                        .description("Api de Cursos de Programação")
-                )
-                .schemaRequirement("JWT_Auth", securityScheme());
+                        .title("API Zeelus")
+                        .description("API para gerenciamento de cuidadores e acompanhados")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Equipe Zeelus")
+                                .email("zeelus.contato@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
--- src/main/java/com/zeelus/zeelus/modules/acompanhado/controller/CadastrarAcompanhadoController.java
+++ src/main/java/com/zeelus/zeelus/modules/acompanhado/controller/CadastrarAcompanhadoController.java
@@ -5,6 +5,12 @@ import com.zeelus.zeelus.modules.acompanhado.dto.CadastrarAcompanhadoDTO;
 import com.zeelus.zeelus.modules.acompanhado.service.CadastrarAcompanhadoSerivce;
 import com.zeelus.zeelus.modules.cuidador.dto.RespostaDTO;
 import com.zeelus.zeelus.modules.acompanhado.dto.CadastrarAcompanhadoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,10 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acompanhado")
+@Tag(name = "Acompanhado", description = "Endpoints para gerenciamento de acompanhados")
 public class CadastrarAcompanhadoController {
     @Autowired
     private CadastrarAcompanhadoSerivce cadastrarAcompanhado;
 
+    @Operation(summary = "Cadastrar acompanhado", description = "Este endpoint realiza o cadastro de um novo acompanhado no sistema")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Acompanhado cadastrado com sucesso",
+            content = @Content(schema = @Schema(implementation = CadastrarAcompanhadoResponseDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos",
+            content = @Content(schema = @Schema(implementation = RespostaDTO.class)))
+    })
     @PreAuthorize("hasRole('CUIDADOR')")
     @PostMapping("/cadastrar")
     private ResponseEntity<Object> cadastrarAcompanhado(@Valid @RequestBody CadastrarAcompanhadoDTO acompanhadoDTO){
